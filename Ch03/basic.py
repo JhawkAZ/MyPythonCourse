d87f77eb2d9b2dbf1b751043aed00d4436351185
Nathan Fields <nathanfields@ccg-services.com>
2019-09-24 09:11:35 -0700

    Input

---
+++ Ch03/basic.py
@@ -0,0 +1,7 @@
+print("Hello and Welcome to a basic python program")
+
+food = input("What is your favorite food? ")
+print("You Like " + food + "? I might like it too")
+
+import sys
+print('Hello, ' + sys.argv[1] + ' and Welcome to a basic python program')
