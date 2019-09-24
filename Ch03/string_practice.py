679efe3f406e3c86bed608f7af1983b1e78c4c49
Nathan Fields <nathanfields@ccg-services.com>
2019-09-24 14:13:47 -0700

    Stings

---
+++ Ch03/string_practice.py
@@ -0,0 +1,15 @@
+print('Lets\'s code some Python')
+print("Let's code some Python")
+print("That's when she said, \"Everyone doesn't understand!\"")
+print("Here are 3 backslahses: \\\\\\")
+name='Francois'
+greeting =f'Bonjour {name}!'
+print(greeting)
+
+
+print("NAME AGE HANDEDNESS SCORE")
+print("Bob 38 L 54.123")
+print("Nancy 28 R 71.12")
+print("NAME \tAGE \tHANDEDNESS \tSCORE")
+print("Bob \t38 \tL \t\t54.123")
+print("Nancy \t28 \tR \t\t71.12")
