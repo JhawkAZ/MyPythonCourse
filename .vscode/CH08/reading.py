8cc7182c8a7f6a1f92bff4a393766d629273771b
Nathan Fields <nathanfields@ccg-services.com>
2019-09-27 08:28:56 -0700

    chapter 6 and 7

---
+++ .vscode/CH08/reading.py
@@ -0,0 +1,3 @@
+with open('hobbies.txt')as hobbies_file:
+    contents = hobbies_file.read()
+print(contents)
---
+++ .vscode/Ch06/capitalize_list.py
@@ -0,0 +1,9 @@
+words=['this','that','the','other']
+
+print(words)
+def capitalize_words(words):
+    """capitalizes the first letters of words"""
+   
+    for i in range(len(words)):
+         words[i] = words{i}.capitalize()        
+print(words)
